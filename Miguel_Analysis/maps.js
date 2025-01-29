1b8499faf27ad9d07a2e16e0a103771dca2adff2
M-Soriano <104050536+M-Soriano@users.noreply.github.com>
2025-01-29 02:15:28 -0800

    added markers to all 50 states

--- Miguel_Analysis/maps.js
+++ Miguel_Analysis/maps.js
@@ -7,6 +7,267 @@ const map = new maplibregl.Map({
     center: [-128.486052, 50.830348],
     zoom: 2.5
     });
+
+    
+    const marker = new maplibregl.Marker()
+        .setLngLat([-89.500000, 44.500000])
+        
+        .addTo(map);
+    const marker2 = new maplibregl.Marker()
+        .setLngLat([-80.500000, 39.000000])
+        
+        .addTo(map);
+    const marker3 = new maplibregl.Marker()
+        .setLngLat([-72.699997, 44.000000])
+        
+        .addTo(map);
+   
+    const marker4 = new maplibregl.Marker()
+        .setLngLat([-100.000000,31.000000])
+        
+        .addTo(map);
+
+    const marker5 = new maplibregl.Marker()
+        .setLngLat([-71.742332,41.742325 ])
+        
+        .addTo(map);
+    
+        const marker6 = new maplibregl.Marker()
+        .setLngLat([-120.500000,44.000000])
+        
+        .addTo(map);
+    const marker7 = new maplibregl.Marker()
+        .setLngLat([-75.000000,43.000000])
+        
+        .addTo(map);
+    const marker8 = new maplibregl.Marker()
+        .setLngLat([-71.500000,44.000000 ])
+        
+        .addTo(map);
+   
+    const marker9 = new maplibregl.Marker()
+        .setLngLat([-100.000000,41.500000 ])
+        
+        .addTo(map);
+    
+     const marker10 = new maplibregl.Marker()
+        .setLngLat([-100.000000,44.500000 ])
+        
+        .addTo(map);
+     const marker11 = new maplibregl.Marker()
+        .setLngLat([-98.000000,38.500000 ])
+        
+        .addTo(map);
+
+     const marker12 = new maplibregl.Marker()
+        .setLngLat([-90.000000,33.000000 ])
+        
+        .addTo(map);
+     const marker13 = new maplibregl.Marker()
+        .setLngLat([-89.000000,40.000000 ])
+        
+        .addTo(map);
+
+     const marker14 = new maplibregl.Marker()
+        .setLngLat([-75.500000,39.000000 ])
+        
+        .addTo(map);
+     const marker15 = new maplibregl.Marker()
+        .setLngLat([-72.699997,41.599998 ])
+        
+        .addTo(map);
+    
+     const marker16 = new maplibregl.Marker()
+        .setLngLat([-92.199997,34.799999 ])
+        
+        .addTo(map);
+    
+    const marker17 = new maplibregl.Marker()
+        .setLngLat([-86.126976,40.273502 ])
+        
+        .addTo(map);
+     
+    const marker18 = new maplibregl.Marker()
+        .setLngLat([-92.603760,38.573936 ])
+        
+        .addTo(map);
+
+    const marker19 = new maplibregl.Marker()
+        .setLngLat([-81.760254,27.994402 ])
+        
+        .addTo(map);
+    
+    const marker20 = new maplibregl.Marker()
+        .setLngLat([-117.224121,39.876019 ])
+        
+        .addTo(map);
+    
+    const marker21 = new maplibregl.Marker()
+        .setLngLat([-68.972168,45.367584 ])
+        
+        .addTo(map);
+
+    const marker22 = new maplibregl.Marker()
+        .setLngLat([-84.506836,44.182205 ])
+        
+        .addTo(map);
+    
+    const marker23 = new maplibregl.Marker()
+        .setLngLat([-83.441162,33.247875 ])
+        
+        .addTo(map);
+    const marker24 = new maplibregl.Marker()
+        .setLngLat([-155.844437,19.741755 ])
+        
+        .addTo(map);
+    const marker25 = new maplibregl.Marker()
+        .setLngLat([-153.369141,66.160507 ])
+        
+        .addTo(map);
+
+    const marker26 = new maplibregl.Marker()
+        .setLngLat([-86.660156 , 35.860119])
+        
+        .addTo(map);
+    
+    const marker27 = new maplibregl.Marker()
+        .setLngLat([ -78.024902,37.926868 ])
+        
+        .addTo(map);
+
+    const marker28 = new maplibregl.Marker()
+        .setLngLat([-74.871826 ,39.833851 ])
+        
+        .addTo(map);
+    
+    const marker29 = new maplibregl.Marker()
+        .setLngLat([-84.270020 , 37.839333])
+        
+        .addTo(map);
+    
+    const marker30 = new maplibregl.Marker()
+        .setLngLat([-100.437012 ,47.650589 ])
+        
+        .addTo(map);
+    
+    const marker49 = new maplibregl.Marker()
+        .setLngLat([-94.636230 ,46.392410 ])
+        
+        .addTo(map);
+    
+    const marker50 = new maplibregl.Marker()
+        .setLngLat([-96.921387 ,36.084621 ])
+        
+        .addTo(map);
+    
+    const marker31 = new maplibregl.Marker()
+        .setLngLat([-109.533691 , 46.965260])
+        
+        .addTo(map);
+    
+    const marker32 = new maplibregl.Marker()
+        .setLngLat([-120.740135 , 47.751076])
+        
+        .addTo(map);
+    
+    const marker33 = new maplibregl.Marker()
+        .setLngLat([-111.950684 , 39.419220])
+        
+        .addTo(map);
+    
+    const marker34 = new maplibregl.Marker()
+        .setLngLat([-105.358887 ,39.113014 ])
+        
+        .addTo(map);
+    
+    const marker35 = new maplibregl.Marker()
+        .setLngLat([-82.996216 , 40.367474])
+        
+        .addTo(map);
+    
+    const marker36 = new maplibregl.Marker()
+        .setLngLat([-86.902298 , 32.318230])
+        
+        .addTo(map);
+    
+    const marker37 = new maplibregl.Marker()
+        .setLngLat([-93.581543 , 42.032974])
+        
+        .addTo(map);
+    
+    const marker38 = new maplibregl.Marker()
+        .setLngLat([ -106.018066,34.307144 ])
+        
+        .addTo(map);
+    
+    const marker39 = new maplibregl.Marker()
+        .setLngLat([ -81.163727,33.836082 ])
+        
+        .addTo(map);
+    
+    const marker40 = new maplibregl.Marker()
+        .setLngLat([ -77.194527, 41.203323])
+        
+        .addTo(map);
+    
+    const marker41 = new maplibregl.Marker()
+        .setLngLat([-111.093735 ,34.048927 ])
+        
+        .addTo(map);
+    
+    const marker42 = new maplibregl.Marker()
+        .setLngLat([ -76.641273, 39.045753])
+        
+        .addTo(map);
+    
+    const marker43 = new maplibregl.Marker()
+        .setLngLat([ -71.382439, 42.407211])
+        
+        .addTo(map);
+    
+    const marker44 = new maplibregl.Marker()
+        .setLngLat([-119.417931 , 36.778259])
+        
+        .addTo(map);
+    
+    const marker45 = new maplibregl.Marker()
+        .setLngLat([ -114.742043,44.068203 ])
+        
+        .addTo(map);
+    
+    const marker46 = new maplibregl.Marker()
+        .setLngLat([-107.290283 ,43.075970 ])
+        
+        .addTo(map);
+    
+    const marker47 = new maplibregl.Marker()
+        .setLngLat([-80.793457 ,35.782169 ])
+        
+        .addTo(map);
+    
+    const marker48 = new maplibregl.Marker()
+        .setLngLat([ -92.329102,30.391830 ])
+        
+        .addTo(map);
+    
+    
+    
+    
+     
+     
+     
+     
+     
+         
+    
+    
+    
+    
+    
+        
+    
+        
+
     let hoveredStateId = null;
 
     map.on('load', () => {
